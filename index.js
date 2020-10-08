4b574539115c77fb5c391234913c3d0aab7e726f
Billyhelm <billyhelm88@gmail.com>
2020-10-08 14:15:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+function makeHeader(text){
+    let body = document.querySelector(".body")
+    let header = document.createElement('h1')
+    header.textContent = text
+    body.append(header)
+}
+
+function submitData(n, e){
+
+    let configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+          name: n,
+          email: e
+        })
+      };
+
+    return fetch('http://localhost:3000/users', configurationObject)
+      .then(res=>res.json())
+      .then(function(object){
+        makeHeader(object.id)
+      })
+      .catch(function(error){
+        makeHeader(error.message)
+      })
+}
+
+
+submitData("Billy", "email@gmail.com")
