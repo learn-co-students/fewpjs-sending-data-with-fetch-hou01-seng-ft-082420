7c327e29ca1264ca1f071c6be38ecf0c8f00bf8a
breewarren <ambreea.warren@gmail.com>
2020-10-15 15:05:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+
+function submitData(name, email) {
+
+    let data = {
+        name,
+        email
+    }
+
+    let options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+    }
+
+    return fetch( 'http://localhost:3000/users', options)
+    .then(response => response.json())
+    .then(object => document.body.innerHTML = object[ "id" ])
+    .catch( error => document.body.innerHTML = error.message)
+}
